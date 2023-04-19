fde31c0a5766e66263ff9b2625154f3610891bfa
Madison LEFEVRE <lefevremadison.contact@gmail.com>
2023-04-19 14:58:19 +0200

    CORS

--- src/main/java/com/mazio/freeForAllApp/controllers/GameController.java
+++ src/main/java/com/mazio/freeForAllApp/controllers/GameController.java
@@ -19,6 +19,7 @@ import com.mazio.freeForAllApp.model.Game;
 import com.mazio.freeForAllApp.model.Player;
 import com.mazio.freeForAllApp.service.GameService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/")
 public class GameController {
@@ -38,6 +39,7 @@ public class GameController {
         return new ResponseEntity<Player[]>(gameService.game.players, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/start" , consumes = "text/plain;charset=UTF-8",  produces = MediaType.APPLICATION_JSON_VALUE ) 
     public ResponseEntity<Game> startGame(@RequestBody String players) throws JsonMappingException, JsonProcessingException {
         Player[] playerData = gameService.convertRawData(players);
@@ -45,24 +47,28 @@ public class GameController {
         return new ResponseEntity<>(gameService.game, HttpStatus.OK);
     }
     
+    @CrossOrigin
     @PostMapping(value = "/revealCard" , consumes = "text/plain;charset=UTF-8",  produces = MediaType.APPLICATION_JSON_VALUE ) 
     public ResponseEntity<Game> revealCard(@RequestBody String data) throws JsonMappingException, JsonProcessingException {
         gameService.standbyToVotingTime();
         return new ResponseEntity<>(gameService.game, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/lockBets" , consumes = "text/plain;charset=UTF-8",  produces = MediaType.APPLICATION_JSON_VALUE ) 
     public ResponseEntity<Game> lockBets(@RequestBody String data) throws JsonMappingException, JsonProcessingException {
         gameService.votingTimeToFight();
         return new ResponseEntity<>(gameService.game, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/declareWinner" , consumes = "text/plain;charset=UTF-8",  produces = MediaType.APPLICATION_JSON_VALUE ) 
     public ResponseEntity<Game> declareWinner(@RequestBody String winner) throws JsonMappingException, JsonProcessingException {
         gameService.fightToStandby(winner.toUpperCase());
         return new ResponseEntity<>(gameService.game, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/vote" , consumes = "text/plain;charset=UTF-8",  produces = MediaType.APPLICATION_JSON_VALUE ) 
     public ResponseEntity<Game> vote(@RequestBody String data, @RequestParam(name = "id") String id, @RequestParam(name = "bet") String bet) throws JsonMappingException, JsonProcessingException {
         gameService.bet(id,bet);
